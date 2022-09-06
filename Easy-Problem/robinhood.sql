b9cf25f8b85f9ab05c0bc087e1e4622daac29135
Sumeet Kumar Singh <41811254+sumeet860@users.noreply.github.com>
2022-09-06 10:31:41 +0530

    robinhood
    SQL to find the top 3 cities with completed trades in descending order.

---
+++ Easy-Problem/robinhood.sql
@@ -0,0 +1,8 @@
+-- To get completed trades
+
+SELECT city, count(*) total_orders
+from trades join users on trades.user_id = users.user_id
+where status = 'Completed' 
+group by city
+order by count(*) desc
+limit 3
