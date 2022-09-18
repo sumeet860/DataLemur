e6c290df0a16e7d41baacd981dff5bec74968538
Sumeet Kumar Singh <41811254+sumeet860@users.noreply.github.com>
2022-09-18 20:39:11 +0530

    Update linkedin_data_science_skill.sql

--- Easy-Problem/linkedin_data_science_skill.sql
+++ Easy-Problem/linkedin_data_science_skill.sql
@@ -5,3 +5,10 @@ WHERE skill IN ('Python','Tableau', 'Postgresql')
 GROUP BY candidate_id
 ORDER BY candidate_id
 LIMIT 2;
+
+-- More generic solution
+SELECT candidate_id FROM candidates
+WHERE skill IN ('Python','Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
